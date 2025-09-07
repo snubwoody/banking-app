d1e558e829210bd5304047a8fd04abf136eaedd8
waku <wakunguma13@gmail.com>
2025-09-07 12:05:31 -0600

    style: Style improvements to dashboard

--- src/components/AddTransaction.svelte
+++ src/components/AddTransaction.svelte
@@ -19,8 +19,16 @@
         <i class="ph ph-plus-circle"></i>
     {/snippet}
     {#snippet body()}
-        <div class="p-3">
+        <form class="p-3">
             <h6>Add transaction</h6>
-        </div>
+            <label class="space-y-1">
+                <p>Amount</p>
+                <input type="text" name="" id="" class="input-field">
+            </label>
+            <label class="space-y-1">
+                <p>Account type</p>
+                <input type="text" name="" id="" class="input-field">
+            </label>
+        </form>
     {/snippet}
 </Drawer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import type { Account, Transaction } from "../lib/db";
     import CreateAccount from "../components/CreateAccount.svelte";
     import AddTransaction from "../components/AddTransaction.svelte";
+    import AccountsCard from "../components/AccountsCard.svelte";
 	
     // TODO: make global transaction store
 	let accounts: Account[] = $state([]);
@@ -41,7 +42,7 @@
 </script>
 
 <main class="flex h-full">
-    <aside class="max-w-[250px] flex-1 h-full bg-neutral-50 border-l border-neutral-300 p-3 space-y-4">
+    <aside class="max-w-[250px] flex-1 h-full  border-l border-neutral-300 p-3 space-y-4">
         <header class="flex items-center justify-between">
             <p class="text-lg">Accounts</p>
             <CreateAccount/>
@@ -59,30 +60,41 @@
             {/each}
         </ul>
     </aside>
-    <section class="flex-1 p-5">
-        <div class="flex items-center justify-between">
-            <p class="text-lg">Transactions</p>
-            <AddTransaction/>
-        </div>
-        <ul class="transaction-grid">
-            <!--Table heading-->
-            <li>Category</li>
-            <li>Date</li>
-            <li>Account</li>
-            <li>Amount</li>
-            <!--Table rows-->
-            {#each transactions as transaction (transaction.id)}
-                <li>{transaction.category.title}</li>
-                <!--TODO: format date-->
-                <li>{transaction.date}</li>
-                <li>{transaction.account.name}</li>
-                <li>$ {transaction.amount}</li>
-            {/each}
-        </ul>
+    <section class="flex-1 p-5 space-y-3">
+        <section class="rounded-sm shadow-sm space-y-1 p-2.5 bg-white">
+            <p>Total Net Worth</p>
+            <h5>$ 156,242.24</h5>
+        </section>
+        <AccountsCard/>
+        <section class="rounded-sm shadow-sm space-y-1 p-2.5 bg-white">
+            <div class="flex items-center justify-between">
+                <p class="text-lg">Transactions</p>
+                <AddTransaction/>
+            </div>
+            <ul class="transaction-grid">
+                <!--Table heading-->
+                <li>Category</li>
+                <li>Date</li>
+                <li>Account</li>
+                <li>Amount</li>
+                <!--Table rows-->
+                {#each transactions as transaction (transaction.id)}
+                    <li>{transaction.category.title}</li>
+                    <!--TODO: format date-->
+                    <li>{transaction.date}</li>
+                    <li>{transaction.account.name}</li>
+                    <li>$ {transaction.amount}</li>
+                {/each}
+            </ul>
+        </section>
     </section>
 </main>
 
 <style>
+    main{
+        background-color: #F5F6FA;
+    }
+
     .transaction-grid{
         display: grid;
         grid-template-columns: repeat(4,1fr);
